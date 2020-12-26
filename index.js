76deef0e019ccebc258d32013197c3c087061c00
Ross MacDonald <rmac2289@gmail.com>
2020-12-26 05:15:52 -0800

    added match by city route

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-const { ApolloServer, gql } = require('apollo-server');
-const { departments } = require('./data');
+const { ApolloServer, gql } = require("apollo-server");
+const { departments } = require("./data");
 
 const typeDefs = gql`
+  type Department {
+    id: ID
+    agency: String
+    phone: String
+    street: String
+    city: String
+    state: String
+    zip: Int
+  }
 
-    type Department {
-        agency: String
-        phone: String
-        street: String
-        city: String
-        state: String
-        zip: Int
-    }
-
-    type Query {
-        agencies: [Department] 
-        agencies_by_state(state: String): [Department]
-    }
-
+  type Query {
+    agencies: [Department]
+    agencies_by_city(city: String): [Department]
+  }
 `;
 
 const resolvers = {
-    Query: {
-        agencies: () => {
-            return departments;
-        },
-        agencies_by_state: (obj, { state }, context, info) => {
-            const foundStates = departments.filter(v => v.state === state)
-            return foundStates
-        }
-    }
+  Query: {
+    agencies: () => {
+      return departments;
+    },
+    agencies_by_city: (obj, { city }, context, info) => {
+      const matchingAgencies = departments.filter((v) => v.city === city);
+      return matchingAgencies;
+    },
+  },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true });
-
-server.listen({
-    port: process.env.PORT || 4000
-})
-.then(({ url }) => {
-    console.log(`Server started at ${url}`)
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
 });
 
-
-
+server
+  .listen({
+    port: process.env.PORT || 4000,
+  })
+  .then(({ url }) => {
+    console.log(`Server started at ${url}`);
+  });
