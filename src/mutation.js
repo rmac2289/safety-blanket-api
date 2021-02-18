b2de2dbdc8be7d8c3ab9b5f33c695b416a6d3683
Ross MacDonald <rmac2289@gmail.com>
2021-02-18 03:48:24 -0800

    added deleteFavorite resolver

--- src/mutation.js
+++ src/mutation.js
@@ -11,6 +11,10 @@ const typeDefs = gql`
     state: String!
     zip: Int!
   }
+  input DeleteFavorite {
+    agency: String!
+    state: String!
+  }
   type Mutation {
     addAgencies(
       agency: String!
@@ -22,6 +26,7 @@ const typeDefs = gql`
     ): Agency
     addUser(userId: String!): User
     addFavorite(userId: String!, favorites: FavoriteInput): User
+    deleteFavorite(userId: String!, favorite: DeleteFavorite): User
   }
 `;
 
@@ -57,6 +62,35 @@ const resolvers = {
         );
       } catch (error) {
         return error.message;
+      }
+    },
+    deleteFavorite: async (_, user) => {
+      try {
+        let currentUser = await User.findOne({ userId: user.userId });
+        let currentFavs = currentUser.favorites;
+        let idxToDelete;
+        for (let i = 0; i < currentFavs.length; i++) {
+          if (
+            currentFavs[i].agency === user.favorite.agency &&
+            currentFavs[i].state === user.favorite.state
+          ) {
+            idxToDelete = i;
+            break;
+          }
+        }
+        let last = currentFavs.length - 1;
+        [currentFavs[idxToDelete], currentFavs[last]] = [
+          currentFavs[last],
+          currentFavs[idxToDelete],
+        ];
+        currentFavs.pop();
+        console.log("deleted");
+        return User.updateOne(
+          { userId: currentUser.userId },
+          { favorites: currentFavs }
+        );
+      } catch (e) {
+        return e.message;
       }
     },
   },
